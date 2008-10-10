06687397482ef4490fc5deea79320d1467a9d0c4
Eric Allen <ericpallen@gmail.com>
2008-10-09 21:46:00 -0400

    Model stuff. Planning to use XStream for XML input/output

---
+++ src/rpiplanner/model/PlanOfStudy.java
@@ -0,0 +1,28 @@
+package rpiplanner.model;
+
+public class PlanOfStudy {
+	protected String fullname;
+	protected String school;
+	protected String studentID;
+	
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
+	
+	
+}
