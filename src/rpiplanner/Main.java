e4b65dc3e379149a9f9e73a4372d77b944ccb5e3
Eric Allen <ericpallen@gmail.com>
2008-10-10 23:19:05 -0400

    Course database input/output

--- src/rpiplanner/Main.java
+++ src/rpiplanner/Main.java
@@ -7,20 +7,36 @@ package rpiplanner;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import javax.swing.JFrame;
 
 import org.jdesktop.application.Application;
-import javax.swing.*;
+
+import rpiplanner.model.CourseDatabase;
+
+import com.thoughtworks.xstream.XStream;
 
 /**
  *
- * @author epall
+ * @author Eric Allen
  */
-public class Main extends Application{
-    private JFrame mainFrame;
+public class Main extends Application {
+    private MainFrame mainFrame;
+    private static POSController planControl;
+    private static CourseDatabase courseDatabase;
+	private static XStream xs;
 
     @Override
     protected void startup() {
+    	loadCourseDatabase();
         mainFrame = new MainFrame();
+        planControl = new POSController(mainFrame.getPlanCard());
+        planControl.setCourseDatabase(courseDatabase);
+        mainFrame.setController(planControl);
+        
         mainFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         mainFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -30,8 +46,31 @@ public class Main extends Application{
         });
         mainFrame.setVisible(true);
     }
+    
+	protected void loadCourseDatabase(){
+    	try {
+			courseDatabase = (CourseDatabase) xs.fromXML(new FileInputStream("course_database.xml"));
+		} catch (FileNotFoundException e) {
+			courseDatabase = new CourseDatabase();
+		}
+    }
+    
+    @Override
+    protected void shutdown() {
+    	try {
+			xs.toXML(courseDatabase, new FileOutputStream("course_database.xml"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 
-    public static void main(String[] args) {
+    public static CourseDatabase getCourseDatabase() {
+		return courseDatabase;
+	}
+
+	public static void main(String[] args) {
+		xs = new XStream();
         Application.launch(Main.class, args);
     }
 }
---
+++ src/rpiplanner/model/CourseDatabase.java
@@ -0,0 +1,12 @@
+package rpiplanner.model;
+
+import java.util.HashSet;
+
+public class CourseDatabase extends HashSet<Course> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7592879999900432026L;
+
+}
