dbbf4ce8884d31de62b82548e4fdda82be1196af
Michael O'Keefe <okeefm@rpi.edu>
2009-02-15 17:28:20 -0500

    Java interface for the Ruby world

---
+++ src/rpiplanner/Apcredit.java
@@ -0,0 +1,11 @@
+package rpiplanner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: Feb 15, 2009
+ * Time: 5:22:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Apcredit {
+}
