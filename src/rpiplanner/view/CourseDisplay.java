7a475b9035e0af018d882fddcbddc3030f58f3ba
nschulte <schuln3@rpi.edu>
2009-02-17 15:07:25 -0500

    added support to fill in requisite courses

--- src/rpiplanner/view/CourseDisplay.java
+++ src/rpiplanner/view/CourseDisplay.java
@@ -22,16 +22,22 @@ package rpiplanner.view;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.TransferHandler;
 
 import rpiplanner.POSController;
 import rpiplanner.model.Course;
+import rpiplanner.model.RequisiteSet;
+import rpiplanner.model.Term;
 
 import com.jgoodies.forms.factories.FormFactory;
 import com.jgoodies.forms.layout.CellConstraints;
@@ -89,6 +95,31 @@ public class CourseDisplay extends JPanel {
 				handler.exportAsDrag(CourseDisplay.this, e, TransferHandler.MOVE);
 			}
 		});
+		
+		addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent e) 
+			{
+				if(e.getButton() == MouseEvent.BUTTON3)
+				{
+					if (course != null)
+					{
+						JPopupMenu contextMenu = new JPopupMenu();
+						
+						contextMenu.add("Fill Requisites").addActionListener(new ActionListener()
+						{
+							public void actionPerformed(ActionEvent e) 
+							{
+								fillRequisites(course, getTerm(course));
+							}
+						});
+						
+						contextMenu.show(getParent(), e.getX() + 10, e.getY() + 30);
+					}
+				}
+			}
+		});
+		
 		addMouseListener(new MouseAdapter() {
 			public void mouseEntered(final MouseEvent e) {
 				controller.setDetailDisplay(course);
@@ -98,7 +129,6 @@ public class CourseDisplay extends JPanel {
 				controller.setDetailDisplay((Course)null);
 			}
 		});
-		//
 	}
 	
 	public void setText(String text){
@@ -117,5 +147,94 @@ public class CourseDisplay extends JPanel {
 		text.setSize(this.getWidth(), this.getHeight());
 		getComponent(0).paint(g);
 		text.setSize(oldSize);
+	}
+	
+	private void fillRequisites(Course fillCourse, int term)
+	{
+		// for some reason, fillCourse.getPrerequisites() didnt work, but it WOULD work
+		// if i set fillCourse to controller.getCourseDatabase().getCourse(fillCourse.getCatalogNumber())
+		// no idea why
+		// for some courses, controller.getCourseDatabase().getCourse(fillCourse.getCatalogNumber()) returns null
+		// dont know why this happens, either
+		fillCourse = controller.getCourseDatabase().getCourse(fillCourse.getCatalogNumber());	
+		RequisiteSet reqs = fillCourse.getPrerequisites();
+		
+		for (int i = 0; i < reqs.size(); i++)
+		{
+			// if the course isnt offered in both spring and fall, and we arent in the term that its offered in,
+			// go back one term so that we are in the term its offered in
+			if ((fillCourse.getAvailableTerms().length < 2) && (fillCourse.getAvailableTerms()[0] != controller.getPlan().getTerm(term).getTerm()))
+			{
+				term--;
+			}
+			
+			// make sure theres still terms
+			// the ap/transfer term is term 0,
+			// if we dont want to use that then make it (term - 1 >= 1)
+			if (term - 1 >= 0)
+			{
+				fillRequisites(reqs.get(i), term - 1);
+			}
+			
+			// ran out of terms for this line of prereqs
+			else
+			{
+				return;
+			}
+		}
+		
+		// this looks to see if the requisite class were trying to add
+		// has already been added.
+		// not sure if theres an easier way to check or not
+		ArrayList<Term> dupes = controller.getPlan().getTerms();
+		boolean dupeCourse = false;
+		for (int t = 0; t < dupes.size(); t++)
+		{
+			ArrayList<Course> dupeCourses = dupes.get(t).getCourses();
+			
+			for (int dc = 0; dc < dupeCourses.size(); dc++)
+			{
+				if (dupeCourses.get(dc).equals(fillCourse))
+				{
+					dupeCourse = true;
+					break;
+				}
+			}
+		}
+		
+		// add course if no duplicate found
+		if (!dupeCourse)
+		{
+			controller.addCourse(term, fillCourse);
+		}
+		
+		// now do corequisites and their requisites
+		RequisiteSet coreqs = fillCourse.getCorequisites();
+		for (int k = 0; k < coreqs.size(); k++)
+		{
+			fillRequisites(coreqs.get(k), term);
+		}
+	}
+	
+	// this returns the term number that a course is in
+	// maybe should be in POSController or PlanOfStudy class?
+	private int getTerm(Course toFind)
+	{
+		ArrayList<Term> terms = controller.getPlan().getTerms();
+		
+		for (int i = 0; i < terms.size(); i++)
+		{
+			ArrayList<Course> termCourses = terms.get(i).getCourses();
+			
+			for (int k = 0; k < termCourses.size(); k++)
+			{
+				if (toFind.equals(termCourses.get(k)))
+				{
+					return i;
+				}
+			}
+		}
+		
+		return -1; // course not found
 	}
 }
