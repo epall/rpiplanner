1af88bc7dab83dc6512fa14068b1d860103111aa
Michael O'Keefe <okeefm@rpi.edu>
2009-02-24 20:04:44 -0500

    added ap credit GUI beginnings

---
+++ src/rpiplanner/view/apcredit.java
@@ -0,0 +1,15 @@
+package rpiplanner.view;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: Feb 24, 2009
+ * Time: 6:42:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class apcredit {
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+}
