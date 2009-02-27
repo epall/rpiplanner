911bc89ac4e4293a6620cd10855ea1e71e90aee4
Michael O'Keefe <okeefm@rpi.edu>
2009-02-27 14:43:05 -0500

    commited apcredit.rb to get it in sync with GitHub repo

--- apcredit.rb
+++ apcredit.rb
@@ -4,32 +4,32 @@ class apcredit
 
   include Java::Rpiplanner::Apcredit
  -
-def getcourse(test, score)
+def getcourse(tests, scores)
 
 apcourses = []
-  if test == "Computer Science AB"  && [5].include?(score)
+  if test == "Computer Science AB"  && score == 5
   	apcourses << str_to_course("CSCI-1100")
   	apcourses << str_to_course("CHEM-1200")
-  elsif test == "Computer Science AB" && [4].include?(score)
+  elsif test == "Computer Science AB" && score == 4
   	apcourses << str_to_course("CSCI-1100")
-  elsif test == "Computer Science A" && [5].include?(score)
+  elsif test == "Computer Science A" && score == 5
   	apcourses << str_to_course("CSCI-1100")
-  when test == "Government & Politics" && [4,5].include?(score)
+  elsif test == "Government & Politics" && [4,5].include?(score)
   	apcourses << str_to_course("STSS-1000")
-  when test == "Biology" && [5].include?(score)
+  elsif test == "Biology" && score == 5
   	apcourses << str_to_course("BIOL-1010")
-  when test == "Biology" && [4].include?(score)
+  elsif test == "Biology" && score == 4
   	apcourses << str_to_course("BIOL-1000")
-  when test == "Chemistry" && [5].include?(score)
+  elsif test == "Chemistry" && score == 4
   	apcourses << str_to_course("CHEM-1100")
   	apcourses << str_to_course("CHEM-1200")
-  when ((["English Language","English Literature"].include?(test)) && [4,5].include?(score)
+  elsif ((["English Language","English Literature"].include?(test)) && [4,5].include?(score)
   	apcourses << str_to_course("WRIT-1000")
-  when (["American History","European History","World History"].include(test)) && [4,5].include?(score)
+  elsif (["American History","European History","World History"].include(test)) && [4,5].include?(score)
   	apcourses << str_to_course("STSH-1000")
-  when test == "Statistics" && [4,5].include?(score)
+  elsif test == "Statistics" && [4,5].include?(score)
   	apcourses << str_to_course("MGMT-2100")
-  when test == "Psychology" && [4,5].include?(score)
+  elsif test == "Psychology" && [4,5].include?(score)
   	apcourses << str_to_course("PSYC-1200")
 
   apcourses.to_java(Java::RpiplannerModel::Course)
