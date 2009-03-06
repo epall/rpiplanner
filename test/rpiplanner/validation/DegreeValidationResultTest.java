3b803a412a9dc77dc904344dcac41846fd1b89eb
Matthew Murphy <matthew.john.murphy@gmail.com>
2009-03-06 00:35:55 -0500

    Added DegreeTest and DegreeValidationResultTest I want to try test driven development, so I'm going to write some tests then write the code that uses it.  I'm also going to test the encapsulation and hiding of the ValidationResult

---
+++ test/rpiplanner/validation/DegreeValidationResultTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2008 Eric Allen allene2@rpi.edu
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package rpiplanner.validation;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class DegreeValidationResultTest
+{
+    //TODO
+    @Before
+	public void setUp() throws Exception
+    {
+	}
+
+	@After
+	public void tearDown() throws Exception
+    {
+	}
+
+	@Test
+    public void testPercentComplete()
+    {
+        fail("Not yet implemented");
+    }
+    
+}
