206a117bb18c3c2dfdbf450db63f3b6eeb566a44
Matthew Murphy <matthew.john.murphy@gmail.com>
2009-03-05 23:44:00 -0500

    Copyright Update I enabled the copyright plugin on my project.  I am not uploading the project file with this commit as it may cause issues with other developers.  I'm going to send an email to moorthy and ask him about what our copyright should be.  Hopefully we can have a standard copyright that everyone can use soon.

--- src/com/swtdesigner/SwingResourceManager.java
+++ src/com/swtdesigner/SwingResourceManager.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2008 Eric Allen allene2@rpi.edu
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 package com.swtdesigner;
 
 import java.awt.Image;
@@ -11,15 +29,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import javax.swing.ImageIcon;
 
-/**
- * Utility class for managing resources such as colors, fonts, images, etc.
- * 
- * This class may be freely distributed as part of any application or plugin.
- * <p>
- * Copyright (c) 2003 - 2004, Instantiations, Inc. <br>All Rights Reserved
- * 
- * @author scheglov_ke
- */
 public class SwingResourceManager {
 	
 	/**
--- src/rpiplanner/validation/ValidationResult.java
+++ src/rpiplanner/validation/ValidationResult.java
@@ -1,6 +1,25 @@
+/*
+ * Copyright (C) 2008 Eric Allen allene2@rpi.edu
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 package rpiplanner.validation;
 
 import rpiplanner.model.Course;
+import rpiplanner.validation.DegreeValidationResult;
 
 /**
  * A {@link DegreeValidator}'s <code>validate()</code> method will produce a
@@ -21,11 +40,11 @@ public interface ValidationResult {
     /**
      * Get the results for a specific section.
      * @param name A name of a section in the <code>getSections()</code> array of
-     * {@link DegreeValidator}.
+     * {@link rpiplanner.validation.DegreeValidator}.
      * @return The results for the requested section, or <code>null</code>.
      * if it does not exist.
      */
-	public Section getSectionResults(String name);
+	public DegreeValidationResult.Section getSectionResults(String name);
 	
 	public interface Section {
 		public Course[] missingCourses();
