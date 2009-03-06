649b17417543b260df9f68f199c3e64f829648c5
Matthew Murphy <matthew.john.murphy@gmail.com>
2009-03-05 22:09:45 -0500

    Created DegreeValidationResult and Section as per discussion with Eric

---
+++ src/rpiplanner/validation/validation/xml/DegreeValidationResult.java
@@ -0,0 +1,22 @@
+package rpiplanner.validation.validation.xml;
+
+import rpiplanner.validation.ValidationResult;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matt Murphy
+ * Date: Mar 5, 2009
+ */
+public class DegreeValidationResult implements ValidationResult
+{
+	public int percentComplete()
+    {
+        return 0;
+    }
+
+
+	public Section getSectionResults(String name)
+    {
+        return null;
+    }
+}
---
+++ src/rpiplanner/validation/validation/xml/Section.java
@@ -0,0 +1,32 @@
+package rpiplanner.validation.validation.xml;
+
+import rpiplanner.model.Course;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matt Murphy
+ * Date: Mar 5, 2009
+ */
+public class Section
+{
+		public Course[] missingCourses()
+        {
+            return null;
+        }
+		public Course[] appliedCourses()
+        {
+            return null;
+        }
+		public String[] messages()
+        {
+            return null;
+        }
+		public Course[] potentialCourses()
+        {
+            return null;
+        }
+		public boolean isSuccess()
+        {
+            return false;
+        }
+}
