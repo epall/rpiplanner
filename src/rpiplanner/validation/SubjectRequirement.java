91d82dbdfb537f803a7c14b32041be1d76e13a24
Murphy@.(none) <Murphy@.(none)>
2009-03-06 12:19:48 -0500

    Added SubjectRequirement

---
+++ src/rpiplanner/validation/SubjectRequirement.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2008 Eric Allen allene2@rpi.edu
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package rpiplanner.validation;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matt Murphy
+ * Date: Mar 6, 2009
+ * Time: 12:18:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SubjectRequirement
+{
+}
