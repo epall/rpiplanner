15feafac4ec8f5857b7472a964d21ac571bea5b6
unknown <Matt Murphy@.(none)>
2009-03-09 20:09:43 -0400

    Created ISection to use as my section interface.

---
+++ src/rpiplanner/validation/ISection.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2008 Eric Allen allene2@rpi.edu
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package rpiplanner.validation;
+
+import rpiplanner.model.Course;
+public interface ISection {
+    public Course[] missingCourses();
+    public Course[] appliedCourses();
+    public String[] messages();
+    public Course[] potentialCourses();
+    public boolean isSuccess();
+}
+
