a63a66b1a91bac229194f2160651d3ae87c3e204
nschulte <schuln3@rpi.edu>
2009-03-17 19:25:55 -0400

    forgot to add Pair.java in the last commit

---
+++ src/rpiplanner/model/Pair.java
@@ -0,0 +1,38 @@
+/* RPI Planner - Customized plans of study for RPI students.
+ *
+ * Copyright (C) 2008 Eric Allen allene2@rpi.edu
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package rpiplanner.model;
+
+public class Pair<First, Second> {
+    private First first;
+    private Second second;
+    
+    public Pair(First first, Second second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+    public Second getSecond() {
+         return second;
+    }
+ 
+    public First getFirst() {
+        return first;
+    }
+}
