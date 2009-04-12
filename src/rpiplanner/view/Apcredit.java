7fdcacf3e6d8e9123182546cdfa4edf72bf294a0
Michael O'Keefe <okeefm@rpi.edu>
2009-04-12 15:05:46 -0400

    worked on getting the add ap course button to do something. Didn't get very far.

--- src/rpiplanner/view/Apcredit.java
+++ src/rpiplanner/view/Apcredit.java
@@ -3,6 +3,7 @@ package rpiplanner.view;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,26 +17,38 @@ public class Apcredit {
     private JPanel buttons;
     private JComboBox Score;
     private JComboBox Test;
-    private JButton AddApButton;
+    public  static JButton AddApButton;
     private JButton CancelButton;
     private JButton OkButton;
     private JPanel panel1;
 
 
     public Apcredit() {
-        AddApButton.addActionListener(new ActionListener() {
-            public void actionPerformed(final ActionEvent e) {
-            System.out.println("This does something.");
-            }
-        });
+        //AddApButton.addActionListener(new ActionListener() {
+          //  public void actionPerformed(final ActionEvent e) {
+         //   System.out.println("This does something.");
+         //   }
+       // });
         CancelButton.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
-
+            System.out.println("this also does something.");
             }
         });
         OkButton.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
+            }
+        });
+    }
 
+    public static void main(String[] args){
+        final JFrame window = new JFrame("Title");
+        window.setBounds(100, 100, 500, 400);
+        window.getContentPane().setLayout(new BorderLayout());
+        window.getContentPane().add(new Apcredit().panel1, BorderLayout.CENTER);
+        window.setVisible(true);
+        AddApButton.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+            window.getContentPane().add(new Apcredit().tests, BorderLayout.CENTER);
             }
         });
     }
