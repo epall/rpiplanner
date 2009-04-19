d2f7a7b28b7fcf934163b6734758c9c8aef72e86
Michael O'Keefe <okeefm@rpi.edu>
2009-04-19 14:30:04 -0400

    Updated apcredit to be able to create a new apcredit window. Still not quite working, but getting closer...

--- src/rpiplanner/view/Apcredit.java
+++ src/rpiplanner/view/Apcredit.java
@@ -1,5 +1,8 @@
 package rpiplanner.view;
 
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.CellConstraints;
+
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -17,7 +20,7 @@ public class Apcredit {
     private JPanel buttons;
     private JComboBox Score;
     private JComboBox Test;
-    public  static JButton AddApButton;
+    public JButton AddApButton;
     private JButton CancelButton;
     private JButton OkButton;
     private JPanel panel1;
@@ -25,32 +28,39 @@ public class Apcredit {
 
     public Apcredit() {
         //AddApButton.addActionListener(new ActionListener() {
-          //  public void actionPerformed(final ActionEvent e) {
-         //   System.out.println("This does something.");
-         //   }
-       // });
+        //  public void actionPerformed(final ActionEvent e) {
+        //   System.out.println("This does something.");
+        //   }
+        // });
         CancelButton.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
-            System.out.println("this also does something.");
+                System.out.println("this also does something.");
             }
         });
         OkButton.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
             }
         });
+        AddApButton.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                GridBagConstraints c = new GridBagConstraints();
+                c.gridx = 1;
+                c.gridy = 2;
+                c.gridheight = 1;
+                c.gridwidth = 1;
+                tests.add(new JButton("foo"), c);
+            }
+
+        });
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         final JFrame window = new JFrame("Title");
         window.setBounds(100, 100, 500, 400);
         window.getContentPane().setLayout(new BorderLayout());
         window.getContentPane().add(new Apcredit().panel1, BorderLayout.CENTER);
         window.setVisible(true);
-        AddApButton.addActionListener(new ActionListener() {
-            public void actionPerformed(final ActionEvent e) {
-            window.getContentPane().add(new Apcredit().tests, BorderLayout.CENTER);
-            }
-        });
     }
+
 }
 
