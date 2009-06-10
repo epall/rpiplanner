a9759723222be57e5ddc34751024b957058c8834
Matthew Murphy <matthew.john.murphy@gmail.com>
2009-06-10 13:42:52 -0400

    Added a new test degree to the database for furthur testing

--- src/rpiplanner/model/DegreeDatabase.java
+++ src/rpiplanner/model/DegreeDatabase.java
@@ -23,13 +23,16 @@ import rpiplanner.validation.degree.Degree;
 import rpiplanner.validation.requirements.CoreRequirement;
 import rpiplanner.validation.requirements.HumanitiesRequirement;
 import rpiplanner.validation.requirements.FreeElectiveRequirement;
+import rpiplanner.validation.requirements.RestrictedRequirement;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 
-
+@XStreamAlias("DegreeDatabase")
 public class DegreeDatabase {
     ArrayList<Degree> database = new ArrayList<Degree>();
 
     public DegreeDatabase() {
         createTestDegree();
+        createAnotherTestDegree();
     }
 
     private void createTestDegree() {
@@ -60,32 +63,101 @@ public class DegreeDatabase {
         testDegree.addRequirement(freeReq);
 
         database.add(testDegree);
-
-        createAnotherTestDegree();
     }
 
     private void createAnotherTestDegree() {
-        Degree testDegree = new Degree("Test Degree 1", 128);
-        String name = "Test Requirement 1";
-        String desc = "Testing the Core Requirement Validator";
-        CoreRequirement coreMathScienceReq = new CoreRequirement(name, desc);
+        Degree newDegree = new Degree("Computer & Systems Engineering", 128);
+        CoreRequirement coreMathScienceReq = new CoreRequirement("Math and Science Core", 
+		"Must fufill all course requirements below.");
+		
+		coreMathScienceReq.addCourse(Course.get("CSCI","1100"));
+		coreMathScienceReq.addCourse(Course.get("CSCI","1200"));
+		coreMathScienceReq.addCourse(Course.get("CSCI","2300"));
+		coreMathScienceReq.addCourse(Course.get("MATH","1010"));
+		coreMathScienceReq.addCourse(Course.get("MATH","1020"));
+		coreMathScienceReq.addCourse(Course.get("MATH","2400"));
+		coreMathScienceReq.addCourse(Course.get("MATH","2800"));
+		coreMathScienceReq.addCourse(Course.get("PHYS","1100"));
+		coreMathScienceReq.addCourse(Course.get("PHYS","1200"));
+		coreMathScienceReq.addCourse(Course.get("CHEM","1100"));
+		
+		newDegree.addRequirement(coreMathScienceReq);
+		
+		CoreRequirement coreEngineeringReq = new CoreRequirement("Core Engineering Courses",
+				"Must fufill all course requirements below.");
+		
+		coreEngineeringReq.addCourse(Course.get("ENGR","1100"));
+		coreEngineeringReq.addCourse(Course.get("ENGR","2350"));
+		coreEngineeringReq.addCourse(Course.get("ENGR","1200"));
+		coreEngineeringReq.addCourse(Course.get("ENGR","1310"));
+		coreEngineeringReq.addCourse(Course.get("ENGR","2050"));
+		coreEngineeringReq.addCourse(Course.get("ENGR","4010"));
+		
+		newDegree.addRequirement(coreEngineeringReq);
+		
+		CoreRequirement coreMajorReq = new CoreRequirement("Required Major Courses", 
+				"Choose either ECSE 4500 or ENGR 2600 (with advisor's approval).");
+		
+		coreMajorReq.addCourse(Course.get("ECSE","2610"));
+		coreMajorReq.addCourse(Course.get("ECSE","2660"));
+		coreMajorReq.addCourse(Course.get("ECSE","2410"));
+		coreMajorReq.addCourse(Course.get("ECSE","4500"));
+		coreMajorReq.addReplacementCourse(Course.get("ECSE","4500"),
+				Course.get("ENGR","2600"));
+		
+		newDegree.addRequirement(coreMajorReq);
+		
+		RestrictedRequirement newRestReq;
 
-        coreMathScienceReq.addCourse(Course.get("CSCI","1100"));
-        coreMathScienceReq.addCourse(Course.get("CSCI","1200"));
-        coreMathScienceReq.addCourse(Course.get("CSCI","2300"));
-        coreMathScienceReq.addCourse(Course.get("MATH","1010"));
-        coreMathScienceReq.addCourse(Course.get("MATH","1020"));
-        coreMathScienceReq.addCourse(Course.get("MATH","2400"));
-        coreMathScienceReq.addCourse(Course.get("MATH","2800"));
-        coreMathScienceReq.addCourse(Course.get("PHYS","1100"));
-        coreMathScienceReq.addCourse(Course.get("PHYS","2400"));
-        coreMathScienceReq.addCourse(Course.get("CHEM","1100"));
+		String restReqDesc = "Choose one of the following courses.";
+		String restReqName = "Multidisciplinary Engineering Elective";
+		int restReqNumClasses = 1;
+		int restReqNumCredits = 4;
+		newRestReq = new RestrictedRequirement(restReqName,
+				restReqDesc);
+        newRestReq.setNumCourses(restReqNumClasses);
+        newRestReq.setNumCredits(restReqNumCredits);
+		newRestReq.addCourse(Course.get("ENGR","1600"));
+		newRestReq.addCourse(Course.get("ENGR","2090"));
+		newRestReq.addCourse(Course.get("ENGR","2550"));
+		newRestReq.addCourse(Course.get("ENGR","2530"));
+		
+		
+		newDegree.addRequirement(newRestReq);
+		
+		restReqName = "Software Engineering Elective";
+		restReqDesc = "Choose one of the following courses.";
+		restReqNumClasses = 1;
+		restReqNumCredits = 3;
+	    newRestReq = new RestrictedRequirement(restReqName,
+				restReqDesc);
+        newRestReq.setNumCourses(restReqNumClasses);
+        newRestReq.setNumCredits(restReqNumCredits);
+		newRestReq.addCourse(Course.get("ECSE","4690"));
+		newRestReq.addCourse(Course.get("ECSE","4750"));
+		newRestReq.addCourse(Course.get("CSCI","4380"));
+		newRestReq.addCourse(Course.get("CSCI","4440"));
+		newRestReq.addCourse(Course.get("CSCI","4600"));
 
-        coreMathScienceReq.addReplacementCourse(Course.get("MATH","1010"), Course.get("ARTS","1010"));
+        newDegree.addRequirement(newRestReq);
+		
+		
+		restReqName = "Design Elective";
+		restReqDesc = "Choose one of the following courses.";
+		restReqNumClasses = 1;
+		restReqNumCredits = 3;
+		newRestReq = new RestrictedRequirement(restReqName, 
+				restReqDesc);
+        newRestReq.setNumCourses(restReqNumClasses);
+        newRestReq.setNumCredits(restReqNumCredits);
+		newRestReq.addCourse(Course.get("ECSE","4780"));
+		newRestReq.addCourse(Course.get("ECSE","4900"));
+		newRestReq.addCourse(Course.get("MANE","4220"));
+		newRestReq.addCourse(Course.get("EPOW","4850"));
 
-        testDegree.addRequirement(coreMathScienceReq);
+        newDegree.addRequirement(newRestReq);
 
-        database.add(testDegree);
+        database.add(newDegree);
     }
 
     public Degree[] listDegrees() {
