0e5126c41bc8e70f527b8937de5339e6bfc38732
Michael O'Keefe <okeefm@rpi.edu>
2010-03-06 12:05:02 -0500

    removed old file

--- degrees/binf.rb
+++
@@ -1,9 +0,0 @@
-degree "B.S. Bioinformatics and Molecular Biology 2012", 2 do |d|
-  d.section "Communication Requirement" do |s|
-    s.valid_courses do |course|
-      course.description =~ /(communication|writing)-intensive/i
-    end
-    s.exclusive = false
-    s.count = 2
-  end
-  
