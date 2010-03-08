62a31e1cb8219c17250c299895079cac62408864
Eric Allen <ericpallen@gmail.com>
2010-03-07 21:11:26 -0500

    Insanely fast rpicatalog using the Typhoeus gem

--- ripcatalog.rb
+++ ripcatalog.rb
@@ -1,43 +1,53 @@
 require 'rubygems'
+require 'typhoeus'
 require 'hpricot'
 require 'net/http'
 require 'uri'
 require 'fileutils'
 
+$hydra = Typhoeus::Hydra.new(:max_concurrency => 5)
+
 def pull_dept(department)
-  req = Net::HTTP::Post.new('http://catalog.rpi.edu/content.php?catoid=5&navoid=111')
-  req.set_form_data({'filter[27]' => department,
-        'filter[29]' => '', 'cpage' => 1, 'cur_cat_oid' => 5, 'filter[32]' => 1,
-        'search_database' => 'Filter', 'filter[keyword]' => ''})
+  $stderr.puts "Pulling #{department}"
+  req = Typhoeus::Request.new('http://catalog.rpi.edu/content.php?catoid=5&navoid=111',
+                              :params => {'filter[27]' => department,
+                                'filter[29]' => '', 'cpage' => 1, 'cur_cat_oid' => 5, 'filter[32]' => 1,
+                                'search_database' => 'Filter', 'filter[keyword]' => ''})
 
-  doc = Hpricot(Net::HTTP.new('catalog.rpi.edu').start {|http| http.request(req) }.body)
-
-  courses = []
-
-  doc.search("a:not(.footer) [@target=\"_blank\"]").each do |c|
-    onclick = c.get_attribute('onclick')
-    coid = onclick.match('showCourse\(\'5\', \'(.*)\',this.*')[1]
-    name = c.inner_html
-    courses << coid
-  end
-  
-  if doc.search("a[@href=\"javascript:course_search(2);\"]")
-    req = Net::HTTP::Post.new('http://catalog.rpi.edu/content.php?catoid=5&navoid=111')
-    req.set_form_data({'filter[27]' => department,
-          'filter[29]' => '', 'cpage' => 2, 'cur_cat_oid' => 5, 'filter[32]' => 1,
-          'search_database' => 'Filter', 'filter[keyword]' => ''})
-
-    doc = Hpricot(Net::HTTP.new('catalog.rpi.edu').start {|http| http.request(req) }.body)
+  req.on_complete do |response|
+    FileUtils.mkdir('catalog/'+department) rescue nil
+    $stderr.puts "Parsing #{department}"
+    doc = Hpricot(response.body)
 
     doc.search("a:not(.footer) [@target=\"_blank\"]").each do |c|
       onclick = c.get_attribute('onclick')
       coid = onclick.match('showCourse\(\'5\', \'(.*)\',this.*')[1]
       name = c.inner_html
-      courses << coid
+      yield coid
+    end
+
+    if doc.search("a[@href=\"javascript:course_search(2);\"]")
+      page2 = Typhoeus::Request.new('http://catalog.rpi.edu/content.php?catoid=5&navoid=111',
+                                    :method => :post, :params => {'filter[27]' => department,
+                                    'filter[29]' => '', 'cpage' => 2, 'cur_cat_oid' => 5, 'filter[32]' => 1,
+                                    'search_database' => 'Filter', 'filter[keyword]' => ''})
+
+      page2.on_complete do |response|
+        $stderr.puts "Parsing 2nd page of #{department}"
+        doc = Hpricot(response.body)
+
+        doc.search("a:not(.footer) [@target=\"_blank\"]").each do |c|
+          onclick = c.get_attribute('onclick')
+          coid = onclick.match('showCourse\(\'5\', \'(.*)\',this.*')[1]
+          name = c.inner_html
+          yield coid
+        end
+      end
+      $hydra.queue page2
     end
   end
 
-  return courses
+  $hydra.queue req
 end
 
 all_departments = ["ARCH", "ARTS", "ASTR", "BCBP", "BIOL", "BMED", "CHEM", "CHME", "CISH", 
@@ -48,18 +58,24 @@ all_departments = ["ARCH", "ARTS", "ASTR", "BCBP", "BIOL", "BMED", "CHEM", "CHME
 
 FileUtils.mkdir('catalog') rescue nil
 
-all_departments.each do |dept|
-  unless File.exist?('catalog/'+dept)
-    pull_dept(dept).each do |coid|
-      FileUtils.mkdir('catalog/'+dept) rescue nil
-      contents = Net::HTTP.get(URI.parse("http://catalog.rpi.edu/preview_course.php?catoid=5&coid=#{coid}&print"))
-      doc = Hpricot(contents)
-      fulltitle = doc.search("td h1").inner_text
-      catalogNumber = fulltitle.match('(.*) - (.*)')[1].sub(' ', '-')
-      out = File.open("catalog/#{dept}/#{catalogNumber}.html", 'w')
-      out.write(contents)
-      out.close
-      puts "Downloading #{catalogNumber}"
+all_departments.each_slice(3) do |depts|
+  depts.each do |dept|
+    unless File.exist?('catalog/'+dept)
+      pull_dept(dept) do |coid|
+        #$stderr.puts "fetching #{coid}"
+        req = Typhoeus::Request.new("http://catalog.rpi.edu/preview_course.php?catoid=5&coid=#{coid}&print")
+        req.on_complete do |response|
+          doc = Hpricot(response.body)
+          fulltitle = doc.search("td h1").inner_text
+          catalogNumber = fulltitle.match('(.*) - (.*)')[1].sub(' ', '-')
+          #$stderr.puts "Dumping #{catalogNumber}"
+          out = File.open("catalog/#{dept}/#{catalogNumber}.html", 'w')
+          out.write(response.body)
+          out.close
+        end
+        $hydra.queue req
+      end
     end
   end
+  $hydra.run
 end
